adf97eabac52eccdd69fbe20a72748764b0e1d8a
Marie Lamoureux <69575238+lamoureuxmarie@users.noreply.github.com>
2020-12-10 19:19:03 +0100

    Added URL to GitHub repo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
     <h1>Weather App</h1>
+    <footer>
+    Open-source code by{" "}
+    <a hRef="https://github.com/lamoureuxmarie/react-weather-app" Target="_blank">Marie Lamoureux</a>
+    </footer>
     </div>
   );
 }
